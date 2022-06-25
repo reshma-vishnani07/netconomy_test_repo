d6d2cab7f34706e1e38b56821a5f749f99107ac3
reshma-vishnani07 <imwatim@123>
2022-06-25 22:30:31 +0530

    Dummy commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -13,6 +13,7 @@ public class BinarySearch {
 		for(int i=0; i<arr.length; i++) {
 			System.out.println(arr[i]);
 		}
+
 		System.out.println(arr[arr.length/2]);
 		int mid =arr[arr.length/2];
 		int temp;
