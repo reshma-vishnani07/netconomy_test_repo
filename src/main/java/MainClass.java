57673fe3fb79e4581ceb13709726a9ea5a5d21ff
reshma-vishnani07 <imwatim@123>
2022-06-26 13:19:09 +0530

    clean

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
 public class MainClass {
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+
 }
